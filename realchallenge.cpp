992f32f663381a220f4606380c0c8abbcc652fb2
Apuroop Paleti <apuroop.paleti@gmail.com>
2017-03-11 08:30:14 +0100

    Create realchallenge.cpp

---
+++ realchallenge.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    long long int x;
+    cin >> x;
+
+    cout << setprecision(50) << sqrt(x)*4.0 << endl;
+}
